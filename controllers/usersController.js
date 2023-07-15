f3ecb0328d8e5e7e53a4e1e56fc6b2ef7009d33d
dipolack <pol22007@byui.edu>
2023-07-14 21:55:11 -0400

    unnecesary code

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,6 @@
 const mongoDB = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
-// exports.getUser = (req, res) => {
-//   const userId = req.params.userId;
-// };
-
-// exports.createUser = (req, res) => {
-//   const userId = req.params.userId;
-//   const taskData = req.body;
-// };
-
-// Get all the 'users' data
 const getAllUsers = async (req, res, next) => {
   try {
       const result = await mongoDB.getDb().db().collection('users').find();
