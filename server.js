fada222dd6b043342fa51b8d174263509a1c7234
Nicole Murff <mur20031@byui.edu>
2023-07-17 23:42:26 -0500

    attempt at OAuth

--- server.js
+++ server.js
@@ -30,3 +30,30 @@ mongodb.initDb((err, mongodb) => {
     console.log("Database connected");
   }
 });
+
+// OAuth
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+// Check to see if someone is logged in
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+})
+
+app.use('/', require('./routes'));
